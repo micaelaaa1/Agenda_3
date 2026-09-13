42fcba593287aa4df9b405c01e67fbc3ecd58fda
micaelaaa1 <micaela.benites0777@gmail.com>
2026-09-13 15:44:50 -0300

    Add files via upload

---
+++ SolucionCapas.Negocio/ContactoNegocio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Agenda3.Entidades;
+using SolucionCapas.Datos;
+
+namespace SolucionCapas.Negocio
+{
+    public class ContactoNegocio
+    {
+        private ContactoDatos _datos =
+            new ContactoDatos();
+
+
+        public Contacto BuscarPorDni(string dni)
+        {
+            if (string.IsNullOrWhiteSpace(dni))
+                return null;
+
+            return _datos.BuscarPorDni(dni);
+        }
+
+
+        public bool Agregar(Contacto contacto)
+        {
+            if (contacto == null)
+                return false;
+
+            if (string.IsNullOrWhiteSpace(contacto.DNI))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(contacto.Apellido))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(contacto.Nombres))
+                return false;
+
+            if (contacto.CuentaCte != null &&
+                contacto.CuentaCte.LimiteCredito < 0)
+                return false;
+
+            return _datos.Agregar(contacto);
+        }
+
+
+        public bool Modificar(Contacto contacto)
+        {
+            if (contacto == null)
+                return false;
+
+            if (string.IsNullOrWhiteSpace(contacto.DNI))
+                return false;
+
+            return _datos.Modificar(contacto);
+        }
+
+
+        public bool Eliminar(string dni)
+        {
+            if (string.IsNullOrWhiteSpace(dni))
+                return false;
+
+            return _datos.Eliminar(dni);
+        }
+    }
+}
